3aff8ff4865a998a767796e52951dd17b3e3f8e5
Rafael Brendo <rafalive122@hotmail.com>
2024-03-13 15:53:33 -0300

    Criando assinatura das funções principais.

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -18,8 +18,14 @@ const state ={
     },
 };
 
+const playerSides = {
+    player1: "player-field-card",
+    computer: "computer-field-card",
+}
+
 const pathImages = ".src/assets/icons/";
 
+//Enumeração
 const cardData = [
     {
         id:0,
@@ -47,7 +53,20 @@ const cardData = [
     },
 ];
 
+async function drawCards(cardNumbers, fieldSide){
+    for(let i=0; i<cardNumbers; i++){
+        const randomIdCard = await getRandomCardId();
+        const cardImage = await createCardImage(randomIdCard, fieldSide);
+   
+        document.getElementById(fieldSide).appendChild(cardImage);
+   
+    }
+}
 
-function init(){}
+function init(){
+    drawCards(5, playerSides.player1);
+    drawCards(5, playerSides.computer);
+
+}
 
 init();
