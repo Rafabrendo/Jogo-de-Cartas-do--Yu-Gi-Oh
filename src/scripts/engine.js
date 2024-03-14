936614eceb75250173bdb9df4069295547de6d57
Rafael Brendo <rafalive122@hotmail.com>
2024-03-14 10:26:34 -0300

    Implementando efeitos sonoros

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -110,24 +110,26 @@ async function updateScore(){
 }
 
 async function checkDuelResults(playerCardId, computerCardId){
-    let duelResults = "Empate";
+    let duelResults = "Draw";
     let playerCard = cardData[playerCardId];
 
     if(playerCard.WinOf.includes(computerCardId)){
-        duelResults = "Ganhou";
+        duelResults = "win";
         state.score.playerScore++;
     }
 
     if(playerCard.LoseOf.includes(computerCardId)){
-        duelResults = "Perdeu";
+        duelResults = "lose";
         state.score.computerScore++;
     }
+
+    await playAudio(duelResults);
 
     return duelResults;
 }
 
 async function drawButton(text){
-    state.actions.button.innerText = text;
+    state.actions.button.innerText = text.toUpperCase();
     state.actions.button.style.display = "block";
 }
 
@@ -155,6 +157,27 @@ async function drawCards(cardNumbers, fieldSide){
 
         document.getElementById(fieldSide).appendChild(cardImage);
    
+    }
+}
+
+async function resetDuel(){
+    state.cardSprites.avatar.src = "";
+    state.actions.button.display = "none";
+
+    state.fieldCards.player.style.display = "none";
+    state.fieldCards.computer.style.display = "none";
+
+    init();
+}
+
+async function playAudio(status){
+    const audio = new Audio(`./src/assets/audio/${status}.wav`)
+    
+    try{
+        audio.play();
+
+    }catch{
+
     }
 }
 
